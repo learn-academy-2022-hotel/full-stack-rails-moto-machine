f3d0b853498942cb47a1ad3177384f4ff46701a0
TorinJacobs <potours@yahoo.com>
2023-01-19 10:59:35 -0800

    completed story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   ### INDEX ###
-  get 'blogs' => 'blog#index'
-
+  get 'blogs' => 'blog#index', as: 'blogs'
+  ### SHOW ###
+  get 'blogs/:id' => 'blog#show', as: 'blog'
   # root "articles#index"
 end
